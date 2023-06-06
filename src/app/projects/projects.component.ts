b0954b13ffc1d602ab0cdf1b080dd7a4504c0d80
Laguerre91 <aguerre.lara@gmail.com>
2023-06-06 14:42:11 +0200

    correction of projects section

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -9,33 +9,13 @@ import * as AOS from 'aos';
 })
 export class ProjectsComponent implements OnInit {
 
-  public weatherApp: boolean;
-  public meltingApp: boolean;
 
-  constructor() {
-    this.weatherApp = true;
-    this.meltingApp = true;
-  }
 
   ngOnInit(): void {
     AOS.init();
 
   }
 
-  setDescription() {
-    this.weatherApp = false;
-  }
-  setDescriptionMelt() {
-    this.meltingApp = false;
-  }
-
-  backWeather() {
-    this.weatherApp = true;
-  }
-
-  backMelt() {
-    this.meltingApp = true;
-  }
 
 
 }
